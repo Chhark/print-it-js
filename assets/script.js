ee718079d44a68e4676f1ca4abd960c067fda3c8
Chhark <kieran.2002.30@gmail.com>
2024-05-02 10:05:31 +0200

    slider fonctionnel

--- assets/script.js
+++ assets/script.js
@@ -16,14 +16,35 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
-
+let current_dot
+let prev_dot 
+let banner_img = document.querySelector(".banner-img")
+banner_img.src = './assets/images/slideshow/slide1.jpg'
+let i = 0
+let current_slide = i
 let arrow_left = document.querySelector(".arrow_left")
 let arrow_right = document.querySelector(".arrow_right")
 
 arrow_left.addEventListener("click",function(){
-	console.log("il y a eu un click")	
+	slider(-1 , -1 ,3)	
 })
 
 arrow_right.addEventListener("click",function(){
-	console.log("il y a eu un click") 	
+	slider(1 ,4 ,0)	
 })
+
+function slider (p_n ,max , min){
+	prev_dot = document.getElementById(i)
+	i += p_n
+	console.log(i)
+	if (i == max){
+		i = min
+	}
+	current_slide=i 
+	current_dot = document.getElementById(i)
+	current_dot.classList.add('dot_selected')
+	prev_dot.classList.remove('dot_selected')
+	banner_img.src = './assets/images/slideshow/' + slides[current_slide].image
+	console.log(current_slide)
+
+} 
